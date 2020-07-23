97b14f2c599bfd25ce416b8a7ee91d9122bbfe2c
Lennart Grahl <lennart.grahl@gmail.com>
2020-07-24 01:07:10 +0200

    Let it crash, let it craaaaaaaaaaaaaaaaash!

--- renderer.js
+++ renderer.js
@@ -4,3 +4,20 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+if (typeof self.ServiceWorker !== 'undefined') {
+    (async () => {
+        // Install service worker
+        try {
+            const registration = await navigator.serviceWorker.register(
+                'sw.js',
+                {
+                    scope: './',
+                },
+            );
+        } catch (error) {
+            console.error('Could not register service worker', error);
+        }
+    })();
+} else {
+    console.warn('Environment does not provide a service worker');
+}
